2ea4f1007b3adbafad5a0a233a0f05212d88c457
Anton Kroosh <antonkroosh@gmail.com>
2023-10-06 12:17:50 +0300

    Add pydantic schema and implement file reading

---
+++ app/playstore/schema.py
@@ -0,0 +1,81 @@
+from pydantic import BaseModel, AnyUrl, EmailStr, field_validator, ValidationError
+from datetime import datetime
+from pydantic_extra_types.country import CountryAlpha2
+from typing import Any
+from iso639 import languages
+
+
+class SchemaValidator(BaseModel):
+    package_name: str
+    market_status: str
+    category: str
+    cat_key: str
+    cat_keys: list[str]
+    cat_type: int
+    title: str
+    description: str
+    short_desc: str
+    icon: AnyUrl
+    icon_72: AnyUrl
+    market_url: AnyUrl
+    what_is_new: str
+    downloads: str
+    downloads_min: int
+    downloads_min: int
+    market_update: datetime
+    created: datetime
+    promo_video: AnyUrl
+    promo_video_image: AnyUrl
+    rating: float
+    size: int
+    screenshots: list[AnyUrl]
+    version: str
+    website: AnyUrl
+    privacy_policy: AnyUrl
+    developer: str
+    content_rating: str
+    number_ratings: int
+    ratings_1: int
+    ratings_2: int
+    ratings_3: int
+    ratings_4: int
+    ratings_5: int
+    price_currency: str
+    price_numeric: float
+    price: str
+    price_i18n_countries: list[CountryAlpha2]
+    iap: bool
+    iap_min: float
+    iap_max: float
+    i18n_lang: list[str] #??
+    similar: list[str]
+    from_developer: list[str]
+    badges: list[str]
+    interactive_elements: list[str]
+    content_descriptors: list[str]
+    contains_ads: bool
+    age_approved_by_teachers: str
+    price_i18n: dict[str, Any]
+    i18n: dict[str, Any]
+    app_availability: dict[str, Any]
+    permissions: list
+    sdks: list
+    physical_address: str
+    email: EmailStr
+    stores: dict[str, Any]
+
+    @field_validator('i18n_lang')
+    @classmethod
+    def validate_languages_i18(cls, languages_i18_list: list[str]) -> list[str]:
+        all_languages_codes = [lang.alpha2 for lang in languages.languages if lang.alpha2]
+        incorrect_languages = []
+
+        for language in languages_i18_list:
+            codes = language.split('-')
+            if any(code not in all_languages_codes for code in codes):
+                incorrect_languages.append(language)
+
+        if incorrect_languages:
+            raise ValueError({"incorrect_languages": incorrect_languages})
+
+        return languages_i18_list
---
+++ app/playstore/utils.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+import pandas as pd
+from pandas import Series
+from pydantic_core._pydantic_core import ValidationError
+
+from config import PLAYSTORE_FILE_PATH
+from schema import SchemaValidator
+
+
+def check_data_types(df):
+    def check_and_count_mismatches(row: Series):
+        missing_columns_errors = defaultdict(lambda: 0)
+        wrong_type_errors = defaultdict(lambda: 0)
+        try:
+            SchemaValidator(**row)
+        except ValidationError as e:
+            for error in e.errors():
+                column = error["loc"][0]
+                if error["type"] == "missing":
+                    missing_columns_errors[column] += 1
+                else:
+                    wrong_type_errors[column] += 1
+            print("missing fields:", dict(missing_columns_errors), "wrong types:", dict(wrong_type_errors))
+
+    df.apply(check_and_count_mismatches, axis=1)
+
+    null_values = df.isnull().sum()
+    print("null values for each field:", null_values)
+
+
+if __name__ == "__main__":
+    df = pd.read_json(PLAYSTORE_FILE_PATH, lines=True)
+
+    check_data_types(df)
