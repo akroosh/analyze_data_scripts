43d65738b8b8e591fb801733fa60d7acff3ad596
Anton Kroosh <antonkroosh@gmail.com>
2023-10-11 18:58:41 +0300

    Refactor main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from app.playstore.utils import validate_jsonl as playstore_validate
 from app.downloads.utils import validate_json_file as downloads_validate
-from config import PLAYSTORE_FILE_PATH, DOWNLOAD_FILE_PATH
+from config import PLAYSTORE_FILE_PATH, DOWNLOAD_FILE_PATH, LOGS_PLAYSTORE_FOLDER, LOGS_DOWNLOAD_FOLDER
 
 if __name__ == "__main__":
     print(f"Validating `{PLAYSTORE_FILE_PATH}`")
     playstore_validate(PLAYSTORE_FILE_PATH)
-    print("-"*100)
     print(f"Validating `{DOWNLOAD_FILE_PATH}`")
     downloads_validate(DOWNLOAD_FILE_PATH)
+    print(f"Finished validation. "
+          f"You can find results in the `{LOGS_PLAYSTORE_FOLDER}` and `{LOGS_DOWNLOAD_FOLDER}` folders")
